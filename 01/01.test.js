5240b5417993db758996cf313b8e2800e81ff723
Martin Flodin <martin.flodin@svt.se>
2022-12-02 10:56:21 +0100

    rename

---
+++ 01/01.test.js
@@ -0,0 +1,29 @@
+import { findMostCalories, findTopThreeCalories } from "./01.js";
+
+const testInput = `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+`;
+
+test("finds the largest amount of calories carried", () => {
+  const actual = findMostCalories(testInput);
+
+  expect(actual).toBe(24000);
+});
+
+test("finds the three largest amounts of calories carried", () => {
+  const actual = findTopThreeCalories(testInput);
+
+  expect(actual).toBe(45000);
+});
